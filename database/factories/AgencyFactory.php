8964908a9598488c49ca77846cb8909dac2371af
Renier Dev <renier.trenuela@gmail.com>
2023-08-18 07:18:21 +0800

    update seeder

--- database/factories/AgencyFactory.php
+++ database/factories/AgencyFactory.php
@@ -17,7 +17,7 @@ class AgencyFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->company(),
         ];
     }
 }
