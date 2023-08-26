629c808f494b19e1ebc15eb4b385f56bb8d7cb65
dex <dex.chavez@yahoo.com>
2023-08-26 20:28:29 +0800

    Update WorkerResource.php
    Form Layout Wizard added to worker

--- app/Filament/Resources/WorkerResource.php
+++ app/Filament/Resources/WorkerResource.php
@@ -4,12 +4,14 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\WorkerResource\Pages;
 use App\Models\Worker;
+use Filament\Forms\Components\Card;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -28,56 +30,78 @@ class WorkerResource extends Resource
     {
         return $form
             ->schema([
-                Fieldset::make()->schema([
-                    TextInput::make('first_name')->required(),
-                    TextInput::make('last_name')->required(),
-                    TextInput::make('middle_name')->required(),
-                ]),
-                Fieldset::make()
-                    ->relationship('workerInformation')
+                Card::make()
                     ->schema([
-                        TextInput::make('contact_number'),
-                        DatePicker::make('workerInformation.date_hired')->required(),
-                        TextInput::make('address'),
-                        DatePicker::make('date_birth')->required(),
-                        TextInput::make('place_birth'),
-                        TextInput::make('passport_number'),
-                        TextInput::make('passport_place_issue'),
-                        DatePicker::make('passport_date_issue'),
-                        DatePicker::make('passport_date_expired'),
-                        TextInput::make('elementary'),
-                        TextInput::make('high_school'),
-                        TextInput::make('vocational'),
-                        TextInput::make('college'),
-                        TextInput::make('father_name'),
-                        TextInput::make('father_occupation'),
-                        TextInput::make('mother_name'),
-                        TextInput::make('mother_occupation'),
-                        TextInput::make('spouse_name'),
-                        TextInput::make('spouse_occupation'),
-                        Select::make('gender')->options([
-                            'male' => 'Male',
-                            'female' => 'Female',
-                        ]),
-                        TextInput::make('religion'),
-                        Select::make('civil_status')->options([
-                            'single' => 'Single',
-                            'married' => 'Married',
-                            'divorced' => 'Divorced',
-                            'widowed' => 'Widowed',
-                        ]),
-                        TextInput::make('height')->numeric(),
-                        TextInput::make('weight')->numeric(),
-                        Textarea::make('objectives')
-                            ->rows(10)
-                            ->cols(20),
-                        FileUpload::make('pic_face')
-                            ->image()
-                            ->imageResizeMode('cover'),
-                        FileUpload::make('pic_body')
-                            ->image()
-                            ->imageResizeMode('cover'),
-                    ]),
+                        Wizard::make([
+                            Wizard\Step::make('Name')
+
+                                ->schema([
+                                    TextInput::make('first_name')->required(),
+                                    TextInput::make('last_name')->required(),
+                                    TextInput::make('middle_name')->required(),
+                                ]),
+                            Wizard\Step::make('Location')
+
+                                ->schema([
+                                    TextInput::make('contact_number'),
+                                    DatePicker::make('workerInformation.date_hired')->required(),
+                                    TextInput::make('address'),
+                                    DatePicker::make('date_birth')->required(),
+                                    TextInput::make('place_birth'),
+                                ]),
+                            Wizard\Step::make('Passport')
+                                ->schema([
+                                    TextInput::make('passport_number'),
+                                    TextInput::make('passport_place_issue'),
+                                    DatePicker::make('passport_date_issue'),
+                                    DatePicker::make('passport_date_expired'),
+                                ]),
+                            Wizard\Step::make('Education')
+                                ->schema([
+                                    TextInput::make('elementary'),
+                                    TextInput::make('high_school'),
+                                    TextInput::make('vocational'),
+                                    TextInput::make('college'),
+                                ]),
+                            Wizard\Step::make('Parents Info')
+                                ->schema([
+                                    TextInput::make('father_name'),
+                                    TextInput::make('father_occupation'),
+                                    TextInput::make('mother_name'),
+                                    TextInput::make('mother_occupation'),
+                                    TextInput::make('spouse_name'),
+                                    TextInput::make('spouse_occupation'),
+                                ]),
+                            Wizard\Step::make('Profile Status')
+                                ->schema([
+                                    Select::make('gender')->options([
+                                        'male' => 'Male',
+                                        'female' => 'Female',
+                                    ]),
+                                    TextInput::make('religion'),
+                                    Select::make('civil_status')->options([
+                                        'single' => 'Single',
+                                        'married' => 'Married',
+                                        'divorced' => 'Divorced',
+                                        'widowed' => 'Widowed',
+                                    ]),
+                                    TextInput::make('height')->numeric(),
+                                    TextInput::make('weight')->numeric(),
+                                    Textarea::make('objectives')
+                                        ->rows(10)
+                                        ->cols(20),
+                                ]),
+                            Wizard\Step::make('File Upload')
+                                ->schema([
+                                    FileUpload::make('pic_face')
+                                        ->image()
+                                        ->imageResizeMode('cover'),
+                                    FileUpload::make('pic_body')
+                                        ->image()
+                                        ->imageResizeMode('cover'),
+                                ]),
+                        ])
+                    ])
             ]);
     }
 
