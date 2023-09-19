9057a42bfe8a0fa5b7ad8b5b2f0650e57bd2ccfb
Renier Dev <renier.trenuela@gmail.com>
2023-09-19 08:11:20 +0800

    update

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Country extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'agency_id',
