a984da8a54ed55cccade21416161be0f9c88ce0b
Renier Dev <renier.trenuela@gmail.com>
2023-09-23 23:33:38 +0800

    update

--- resources/views/downloadables/cv.blade.php
+++ resources/views/downloadables/cv.blade.php
@@ -7,7 +7,7 @@
         }
 
         body {
-            background-image: url("{{ $cvTemplate }}");
+            background-image: url("{!! $cvTemplate !!}");
             background-size: cover;
             background-repeat: no-repeat;
             font-family: "Arial Black", Gadget, sans-serif;
@@ -22,13 +22,13 @@
 <body>
 
     <span style="float: right; margin-top: 15%; margin-right: 10%">
-        <img src="{{ $records->getFirstMediaUrl('pic_face') }}" width="120" height="120px" alt="">
+        <img src="{!! $records->getFirstMediaUrl('pic_face') !!}" width="120" height="120px" alt="">
     </span>
 
     <p style="text-align: center; margin-top: 23%">
         <strong>
             <span style='font-size: 22px;'>
-                {{ $records->first_name }} {{ $records->last_name }}
+                {!! $records->first_name !!} {!! $records->last_name !!}
             </span>
         </strong>
     </p>
@@ -37,14 +37,14 @@
         <p style="text-align: left; margin-top: 1px;"><strong><span
                     style="font-family: &quot;Arial Black&quot;, Gadget, sans-serif; font-size: 19px;"></span><span
                     style='font-family: "Arial Black", Gadget, sans-serif; font-size: 19px;'><b><span
-                            style="color: rgb(0, 0, 0); font-size: 14px;"><u>POSITION :</u> {{ $records->position }}
+                            style="color: rgb(0, 0, 0); font-size: 14px;"><u>POSITION :</u> {!! $records->position !!}
                         </span></b></span><br></strong></p>
 
         <p style="text-align: left; margin-top: 10px;"><strong><span
                     style="font-family: &quot;Arial Black&quot;, Gadget, sans-serif; font-size: 19px;"></span><span
                     style='font-family: "Arial Black", Gadget, sans-serif; font-size: 19px;'><b><span
                             style="color: rgb(0, 0, 0); font-size: 14px;"><u>OBJECTIVES :</u> <br><br>
-                            {{ $records->workerInformation->cover_letter }}
+                            {!! $records->workerInformation->cover_letter !!}
                         </span></b></span><br></strong></p>
 
         <p style="text-align: left;"><strong><span
@@ -54,27 +54,27 @@
 
         <ul style="list-style-type: disc;">
             @foreach ($records->skills as $skill)
-                <li style="padding-bottom: 3px;">{{ $skill->description }}</li>
+                <li style="padding-bottom: 3px;">{!! $skill->description !!}</li>
             @endforeach
         </ul>
 
         <p style="text-align: left;"><strong><span
                     style='font-family: "Arial Black", Gadget, sans-serif; font-size: 19px;'><strong><span
-                            style="color: rgb(0, 0, 0); font-size: 14px;"><u>EDUCATION
-                                :</u></span></strong></span></strong></p>
+                            style="color: rgb(0, 0, 0); font-size: 14px;">
+                            <u>EDUCATION:</u></span></strong></span></strong></p>
 
         <ul style="list-style-type: disc;">
             @foreach ($records->education as $item)
                 <li style="padding-top: 5px;">
                     <strong>
                         <span style="font-size: 17px;  text-transform: uppercase;">
-                            {{ $item->level }} -
+                            {!! $item->level !!} -
                         </span>
                     </strong>
-                    <em> {{ $item->title }}</em>
+                    <em> {!! $item->title !!}</em>
                     <br>
-                    {{ \Carbon\Carbon::parse($item->from_date)->format('F j, Y') }} -
-                    {{ \Carbon\Carbon::parse($item->to_date)->format('F j, Y') }}
+                    {!! \Carbon\Carbon::parse($item->from_date)->format('F j, Y') !!} -
+                    {!! \Carbon\Carbon::parse($item->to_date)->format('F j, Y') !!}
                 </li>
             @endforeach
         </ul>
@@ -90,12 +90,12 @@
             @foreach ($records->workHistory()->orderBy('from_date', 'desc')->get() as $history)
                 <li style="padding-top: 5px;">
                     <span style="font-size: 17px;">
-                        <strong>{{ $history->position }}</strong> - {{ $history->address }}
+                        <strong>{!! $history->position !!}</strong> - {!! $history->address !!}
                     </span>
-                    <span>{{ $history->company }}</span>
+                    <span>{!! $history->company !!}</span>
                     <br>
-                    {{ \Carbon\Carbon::parse($history->from_date)->format('F j, Y') }} -
-                    {{ \Carbon\Carbon::parse($history->to_date)->format('F j, Y') }}
+                    {!! \Carbon\Carbon::parse($history->from_date)->format('F j, Y') !!} -
+                    {!! \Carbon\Carbon::parse($history->to_date)->format('F j, Y') !!}
                 </li>
             @endforeach
         </ul>
